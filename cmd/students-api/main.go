668700abf8706892d626f896c113d055a66a6d81
sujaysinhpatil2 <sujaysinhpatil2@gmail.com>
2024-10-05 21:28:28 +0530

    first commit

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/sujaysinhpatil2/students-api/internal/config"
+)
 
 func main() {
-	fmt.Println("Welcome to Students API")
+	// load config
+	cfg := config.MustLoad()
+
+	// database setup
+
+	// setup router
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to students api"))
+	})
+
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	fmt.Println("server started")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal("failed to staart server")
+	}
+
 }
